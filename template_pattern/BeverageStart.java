939f2c9c38de2257241ab086a89b04b62624e86d
kimdonggyu <dkkim1004@hanmail.net>
2025-11-08 10:30:05 +0900

    템플릿 패턴

---
+++ template_pattern/BeverageStart.java
@@ -0,0 +1,14 @@
+public class BeverageStart {
+
+    public static void main(String[] args) {
+        CaffeineBeverage tea = new Tea();
+        CaffeineBeverage coffee = new Coffee();
+
+        System.out.println("\nMaking tea...");
+
+        tea.prepareRecipe();
+
+        System.out.println("\nMaking coffee...");
+        coffee.prepareRecipe();
+    }
+}
---
+++ template_pattern/CaffeineBeverage.java
@@ -0,0 +1,25 @@
+public abstract class CaffeineBeverage {
+    final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        if (customerWantsCondiments())
+            addCondiments();
+    }
+
+    abstract void brew();
+
+    abstract void addCondiments();
+
+    public void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    public void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+    boolean customerWantsCondiments() {
+        return true;
+    }
+}
